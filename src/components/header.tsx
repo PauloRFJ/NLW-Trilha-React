f65c57b3532d5635147b8bc3bf78a06d00e37bfe
Paulo <paulo_freitas-jun1@estudante.sesisenai.org.br>
2024-04-03 21:36:19 -0300

    Aula 1 + Aula 2

---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+// JSX = JavaScript XML
+// TSX = TypeScript XML
+import nlwUnitedIcon from '../assets/nlw-unite-icon.svg'
+import { NavLink } from './nav-link'
+export function Header() {
+    return (
+        <div className='flex items-center gap-5 py-2'>
+            <img src={nlwUnitedIcon} />
+
+            <nav className='flex gap-5 items-center'>
+                <NavLink href="/eventos">Eventos</NavLink>
+                <NavLink href="/participantes">Participantes</NavLink>
+            </nav>
+        </div>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,ts,jsx,tsx}',
+    './pages/**/*.{html,js,ts,jsx,tsx}',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
